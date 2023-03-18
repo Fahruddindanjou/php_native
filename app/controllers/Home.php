d5a93b4667213c199419219c57eadf08bc12041b
fahruddindanjou <muhammadfahruddin76@gmail.com>
2023-03-18 20:50:46 +0700

    assets MVC constants,bootstrap 5.2

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -2,7 +2,10 @@
 
 class Home extends Controller{
     public function index(){
+        $data['judul'] = 'Home';
+        $this->view('layouts/header', $data);
         $this->view('home/index');
+        $this->view('layouts/footer');
     }
     public function about(){
         $this->view('home/about');
